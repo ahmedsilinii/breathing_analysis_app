d1d002a492e647d7e0ae1b9d75a12fc7782dd446
Ahmed Silini <ahmedsilini5@gmail.com>
2025-04-27 20:18:28 +0100

    create theme.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:breathing_analysis_app/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: AppTheme.theme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
