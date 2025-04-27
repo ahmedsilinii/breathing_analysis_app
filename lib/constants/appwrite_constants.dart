41640141640c025708567e168357a6b61c9d3991
Ahmed Silini <ahmedsilini5@gmail.com>
2025-04-27 20:02:38 +0100

    created appwrite_constants file

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '680e7e950034c74c82ae';
+  static const String projectId = '680e72f5003a45449eac';
+  static const String endPoint = 'http://localhost:80/v1';
+}
