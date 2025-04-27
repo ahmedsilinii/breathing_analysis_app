e7042b2ee89b179db98dbb6b4755dcece2c65532
Ahmed Silini <ahmedsilini5@gmail.com>
2025-04-27 20:15:37 +0100

    Create app_theme.dart

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,15 @@
+import 'package:breathing_analysis_app/theme/palette.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData theme = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Pallete.backgroundColor,
+      elevation: 0,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Pallete.blueColor,
+    ),
+  );
+}
