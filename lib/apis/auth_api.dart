21a7fb3ca4d9bba1efdcd6fcdaad06cd9afe3cc6
Ahmed Silini <ahmedsilini5@gmail.com>
2025-04-29 16:53:28 +0100

    created AuthAPI

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,17 +1,26 @@
 import 'package:appwrite/appwrite.dart';
-import 'package:fpdart/fpdart.dart';
+import 'package:breathing_analysis_app/core/core.dart';
 
-abstract class IAuthAPI{
-  Future<Either<Account, String>> signUp({
+// get user account --> Account from appwrite
+// want to access user related data --> model.Account
+
+abstract class IAuthAPI {
+  FutureEither<Account> signUp({
     required String email,
     required String password,
   });
-
-
 }
 
-class AuthAPI implements IAuthAPI{
+class AuthAPI implements IAuthAPI {
+  final Account _account;
 
- 
+  AuthAPI({required Account account}) : _account = account;
 
+  @override
+  FutureEither<Account> signUp({
+    required String email,
+    required String password,
+  }) async {
+    throw UnimplementedError();
+  }
 }
--- lib/core/type_defs.dart
+++ lib/core/type_defs.dart
@@ -2,3 +2,4 @@ import 'package:breathing_analysis_app/core/failure.dart';
 import 'package:fpdart/fpdart.dart';
 
 typedef FutureEither<T> = Future<Either<Failure, T>>;
+typedef FutureEitherVoid = FutureEither<void>;
