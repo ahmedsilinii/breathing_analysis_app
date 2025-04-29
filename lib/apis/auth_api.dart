f2397eb0b7b4e8811b567ce5ab887bc8c8b75a52
Ahmed Silini <ahmedsilini5@gmail.com>
2025-04-29 18:52:42 +0100

    added AuthAPIProvider

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,10 +1,16 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart';
 import 'package:breathing_analysis_app/core/core.dart';
+import 'package:breathing_analysis_app/core/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 
 // get user account --> Account from appwrite
 // want to access user related data --> model.Account (old version) --> User (new version)
+
+final AuthAPIProvider = Provider((ref) {
+  return AuthAPI(account: ref.watch(appwriteAccountProvider));
+});
 
 abstract class IAuthAPI {
   FutureEither<User> signUp({required String email, required String password});
@@ -35,7 +41,4 @@ class AuthAPI implements IAuthAPI {
       return left(Failure(message: e.toString(), stackTrace: stackTrace));
     }
   }
-
-
-
 }
