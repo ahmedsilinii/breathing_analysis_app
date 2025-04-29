5c564a3ceb4805a6feb0a841a213c64b9a3b6cef
Ahmed Silini <ahmedsilini5@gmail.com>
2025-04-29 19:56:40 +0100

    Update main.dart
    ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:breathing_analysis_app/features/auth/view/signup_view.dart';
 import 'package:breathing_analysis_app/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
