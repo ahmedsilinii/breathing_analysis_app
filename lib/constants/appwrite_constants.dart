0f847095e7ffec16f51e5cf05f784c33da4d0dac
Ahmed Silini <ahmedsilini5@gmail.com>
2025-04-29 21:57:16 +0100

    Update appwrite_constants.dart
    android debugging

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,5 +1,5 @@
 class AppwriteConstants {
   static const String databaseId = '680e7e950034c74c82ae';
   static const String projectId = '680e72f5003a45449eac';
-  static const String endPoint = 'http://localhost:80/v1';
+  static const String endPoint = 'http://10.0.2.2:80/v1';
 }
