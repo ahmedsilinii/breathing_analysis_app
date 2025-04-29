ee03001cb4d8137a41cf564501afec434555a055
Ahmed Silini <ahmedsilini5@gmail.com>
2025-04-29 19:38:29 +0100

    Update auth_controller.dart
    create signUP

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,9 +1,19 @@
+import 'package:breathing_analysis_app/apis/auth_api.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AuthController extends StateNotifier<bool> {
-  AuthController() : super(false);
+  final AuthAPI _authAPI;
+  AuthController({required AuthAPI authAPI}) : _authAPI = authAPI, super(false);
   // state = isLoading
 
- 
+  void singUp({
+    required email,
+    required password,
+    required BuildContext context,
+  }) async {
+    state = true;
+    final res = await _authAPI.signUp(email: email, password: password);
+    res.fold((l)=> null, (r)=> print(r.name));
+  }
 }
