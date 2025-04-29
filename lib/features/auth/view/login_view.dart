ff4dd6d95876bb58d0e37b016d2b5543aa79d2f6
Ahmed Silini <ahmedsilini5@gmail.com>
2025-04-29 23:01:06 +0100

    added isLoading logic

--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,3 +1,4 @@
+import 'package:breathing_analysis_app/common/loading_page.dart';
 import 'package:breathing_analysis_app/common/rounded_small_button.dart';
 import 'package:breathing_analysis_app/constants/ui_constants.dart';
 import 'package:breathing_analysis_app/features/auth/controller/auth_controller.dart';
@@ -44,50 +45,56 @@ class _LoginViewState extends ConsumerState<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appbar,
-      body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              AuthField(controller: emailController, hintText: 'Email '),
-              const SizedBox(height: 25),
-              AuthField(controller: passwordController, hintText: 'Password'),
-              const SizedBox(height: 40),
-              Align(
-                alignment: Alignment.topRight,
-                child: RoundedSmallButton(onTap: onLogin, label: 'Done'),
-              ),
-              const SizedBox(height: 20),
-              RichText(
-                text: TextSpan(
-                  text: 'Don\'t have an account?',
-                  style: const TextStyle(
-                    color: Pallete.greyColor,
-                    fontSize: 16,
+    final isLoading = ref.watch(authControllerProvider);
+    return isLoading
+        ? Loader()
+        : Scaffold(
+          appBar: appbar,
+          body: Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  AuthField(controller: emailController, hintText: 'Email '),
+                  const SizedBox(height: 25),
+                  AuthField(
+                    controller: passwordController,
+                    hintText: 'Password',
                   ),
-                  children: [
-                    TextSpan(
-                      text: ' Sign Up',
+                  const SizedBox(height: 40),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: RoundedSmallButton(onTap: onLogin, label: 'Done'),
+                  ),
+                  const SizedBox(height: 20),
+                  RichText(
+                    text: TextSpan(
+                      text: 'Don\'t have an account?',
                       style: const TextStyle(
-                        color: Pallete.blueColor,
+                        color: Pallete.greyColor,
                         fontSize: 16,
-                        fontWeight: FontWeight.bold,
                       ),
-                      recognizer:
-                          TapGestureRecognizer()
-                            ..onTap = () {
-                              Navigator.push(context, SignupView.route());
-                            },
+                      children: [
+                        TextSpan(
+                          text: ' Sign Up',
+                          style: const TextStyle(
+                            color: Pallete.blueColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          recognizer:
+                              TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(context, SignupView.route());
+                                },
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        );
   }
 }
