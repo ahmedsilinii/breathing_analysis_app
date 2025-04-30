f69346e7fcef87b4114762930d5745d91e74f661
Ahmed Silini <ahmedsilini5@gmail.com>
2025-04-30 17:45:38 +0100

    added saveUserData

--- lib/apis/user_api.dart
+++ lib/apis/user_api.dart
@@ -1,10 +1,33 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:breathing_analysis_app/constants/appwrite_constants.dart';
+import 'package:breathing_analysis_app/core/failure.dart';
 import 'package:breathing_analysis_app/core/type_defs.dart';
+import 'package:breathing_analysis_app/models/user_model.dart';
+import 'package:fpdart/fpdart.dart';
 
 abstract class IUserAPI {
-  FutureEitherVoid saveUserData();
+  FutureEitherVoid saveUserData(UserModel userModel);
 }
 
 class UserAPI implements IUserAPI {
+  final Databases _db;
+
+  UserAPI({required Databases db}) : _db = db;
+
   @override
-  FutureEitherVoid saveUserData() {}
+  FutureEitherVoid saveUserData(UserModel userModel) async {
+    try {
+      await _db.createDocument(
+        databaseId: AppwriteConstants.databaseId,
+        collectionId: AppwriteConstants.userCollectionId,
+        documentId: ID.unique(),
+        data: userModel.toMap(),
+      );
+      return right(null);
+    } on AppwriteException catch (e, st) {
+      return left(Failure(message: 'Failed to save user data', stackTrace: st));
+    } catch (e, st) {
+      return left(Failure(message: e.toString(), stackTrace: st));
+    }
+  }
 }
--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -2,4 +2,6 @@ class AppwriteConstants {
   static const String databaseId = '680e7e950034c74c82ae';
   static const String projectId = '680e72f5003a45449eac';
   static const String endPoint = 'http://10.0.2.2:80/v1';
+
+  static const String userCollectionId = '6812524d002ee10cd667';
 }
