fd6ab3fcb7ba43af0bd411f9daab44d0351127ab
Ahmed Silini <ahmedsilini5@gmail.com>
2025-05-11 04:18:17 +0100

    Update create_diagnosis.dart
    organization nation

--- lib/features/diagnosis/views/create_diagnosis.dart
+++ lib/features/diagnosis/views/create_diagnosis.dart
@@ -26,6 +26,38 @@ class _CreateDiagnosisScreenState extends ConsumerState<CreateDiagnosisScreen> {
   final audioRecorder = AudioRecorder();
   String? recordingPath;
 
+  Future<void> _onRecord() async {
+    if (isRecording) {
+      String? filePath = await audioRecorder.stop();
+      if (filePath != null) {
+        setState(() {
+          isRecording = false;
+          recordingPath = filePath;
+        });
+        // ignore: use_build_context_synchronously
+        showSnackBar(context, 'Recording stopped.');
+      }
+    } else {
+      if (await audioRecorder.hasPermission()) {
+        final Directory appDocumentsDir = await getApplicationCacheDirectory();
+        //ken t7eb tsajel kol recording
+        //final String filePath = '${appDocumentsDir.path}/breath_recording_${DateTime.now().millisecondsSinceEpoch}.wav';
+        //sinon override every recording
+        final String filePath = '${appDocumentsDir.path}/breath_recording.wav';
+        await audioRecorder.start(const RecordConfig(), path: filePath);
+        setState(() {
+          isRecording = true;
+          recordingPath = null;
+        });
+        showSnackBar(
+          // ignore: use_build_context_synchronously
+          context,
+          'Recording started. Tap again to stop.',
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final currentUser = ref.watch(currrentUserAccountProvider).value;
@@ -72,40 +104,7 @@ class _CreateDiagnosisScreenState extends ConsumerState<CreateDiagnosisScreen> {
                         ),
                         padding: const EdgeInsets.symmetric(vertical: 16.0),
                       ),
-                      onPressed: () async {
-                        if (isRecording) {
-                          String? filePath = await audioRecorder.stop();
-                          if (filePath != null) {
-                            setState(() {
-                              isRecording = false;
-                              recordingPath = filePath;
-                            });
-                            // ignore: use_build_context_synchronously
-                            showSnackBar(context, 'Recording stopped.');
-                          }
-                        } else {
-                          if (await audioRecorder.hasPermission()) {
-                            final Directory appDocumentsDir =
-                                await getApplicationCacheDirectory();
-                            final String filePath =
-                                '${appDocumentsDir.path}/breath_recording.wav';
-                            await audioRecorder.start(
-                              const RecordConfig(),
-                              path: filePath,
-                            );
-                            print('Recording started: $filePath');
-                            setState(() {
-                              isRecording = true;
-                              recordingPath = null;
-                            });
-                            showSnackBar(
-                              // ignore: use_build_context_synchronously
-                              context,
-                              'Recording started. Tap again to stop.',
-                            );
-                          }
-                        }
-                      },
+                      onPressed: _onRecord,
                       icon: Icon(isRecording ? Icons.stop : Icons.mic),
                       label: const Text('Record Breath'),
                     ),
