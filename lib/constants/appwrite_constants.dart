e6296dfb6adad4980613dea2e193033e8147e9db
Ahmed Silini <ahmedsilini5@gmail.com>
2025-05-12 07:35:37 +0100

    added methods to extract file urls

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -9,4 +9,15 @@ class AppwriteConstants {
   static const String medicalReportsBuckedId = '6820d28f0023e13cd053';
   static const String breathingRecordsBucketId = '6820d2a300233b5371d3';
 
+  static String fileURL(String fileId, String bucketId) =>
+      'http://localhost/v1/storage/buckets/$bucketId/files/$fileId/view?project=$projectId&project=$projectId&mode=admin';
+
+  // Example URLs for testing
+  // medical reports
+  //'http://localhost/v1/storage/buckets/6820d28f0023e13cd053/files/68219485003ad9be15ab/view?project=680e72f5003a45449eac&project=680e72f5003a45449eac&mode=admin';
+  // breathing records
+  // http://localhost/v1/storage/buckets/6820d2a300233b5371d3/files/682195cc0012a341b2d7/view?project=680e72f5003a45449eac&project=680e72f5003a45449eac&mode=admin
+
+  static String fileURLWithMode(String fileId, String bucketId, String mode) =>
+      'http://localhost/v1/storage/buckets/$bucketId/files/$fileId/view?project=$projectId&project=$projectId&mode=$mode';
 }
